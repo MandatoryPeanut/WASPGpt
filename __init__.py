689349008b4bf131bc58453fa057effe4f8f27be
Dylon <dylonkimbrough@yahoo.com>
2024-02-11 01:39:03 -0600

    Flask tutorial

--- __init__.py
+++ __init__.py
@@ -10,8 +10,6 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'WASPGpt.sqlite')
     )
 
-
-
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
@@ -26,8 +24,10 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
