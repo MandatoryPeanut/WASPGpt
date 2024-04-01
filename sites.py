0ee0558101f4379a07aff46036f2fb303b38993e
Dylon <dylonkimbrough@yahoo.com>
2024-04-01 01:31:46 -0500

    Added edit and delete, fixed typos

--- sites.py
+++ sites.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.security import generate_password_hash
+from werkzeug.exceptions import abort
 
 from .auth import login_required
 from .db import get_db
@@ -68,19 +69,22 @@ def register(user_type):
             Manager = request.form['Manager']
             JobSite = request.form['JobSite']
 
-            if not firstName or not lastName or not Salary or not Gender or not DOB or not Manager or not JobSite:
-                error = 'You are missing one or more important fields of information.'
             db = get_db()
-            try:
+            existing_emp = db.execute(
+                "SELECT EmployeeFirstName, EmployeeLastName FROM Employee "
+                "WHERE EmployeeFirstName = ? AND EmployeeLastName = ?;", (firstName, lastName)
+            ).fetchall()
+            if existing_emp is not None:
+                error = f"Employee {firstName, lastName} already exists."
+                flash(error)
+            else:
                 db.execute(
                     "INSERT INTO Employee (EmployeeFirstName, EmployeeLastName, Salary, Gender, DOB, Manager, JobSite) "
                     "VALUES (?, ?, ?, ?, ?, ?, ?)",
                     (firstName, lastName, Salary, Gender, DOB, Manager, JobSite)
                 )
                 db.commit()
-            except db.IntegrityError:
-                error = f"Employee {firstName, ' ', lastName} is already registered."
-            else:
+                flash("Employee registered successfully.")
                 return redirect(url_for("sites.index"))
 
         elif user_type == 'manager':
@@ -119,6 +123,7 @@ def register(user_type):
                 db.commit()
                 flash("Job Site successfully created.")
                 return redirect(url_for("sites.index"))
+
         if error is None:
             flash("Registration successful.")
             return redirect(url_for("sites.index"))
@@ -126,27 +131,7 @@ def register(user_type):
     return render_template('sites/register.html', user_type=user_type)
 
 
-@bp.route('/create', methods=['POST'])
-@login_required
-def create():
-    if request.method == 'POST':
-        Name = request.form['Name']
-        error = None
-        db = get_db()
-        existing_site = db.execute("SELECT Name FROM JobSite VALUES ?;", (Name,))
-        if existing_site is not None:
-            error = f"Job Site with the name {Name} already exists."
-            flash(error)
-        else:
-            db.execute("INSERT INTO JobSite VALUES ?;", (Name,))
-            db.commit()
-            flash("Job Site successfully created.")
-            return redirect(url_for("sites.index"))
-    else:
-        return render_template('sites/create.html')
-
-
-@bp.route('/display/<int:jobSiteID>', methods=['GET'])
+@bp.route('/display/<int:jobSiteID>', methods=['GET', 'POST'])
 @login_required
 def display(jobSiteID):
     db = get_db()
@@ -166,3 +151,55 @@ def display(jobSiteID):
     return render_template(
         'sites/display.html', Employees=Employees, Income=Income,
         Expenditure=Expenditure, jobSiteID=jobSiteID, Name=Name)
+
+
+@bp.route('/edit/<int:id>', methods=['POST', 'GET'])
+@login_required
+def edit(id):
+    data = getEmployeeData(id)
+    if request.method == 'POST':
+        firstName = request.form['EmployeeFirstName']
+        lastName = request.form['EmployeeLastName']
+        Salary = request.form['Salary']
+        Gender = request.form['Gender']
+        DOB = request.form['DOB']
+        Manager = request.form['Manager']
+        JobSite = request.form['JobSite']
+
+        error = None
+
+        if not firstName or not lastName or not Salary or not Gender or not DOB or not Manager or not JobSite:
+            error = 'Missing important information.'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "UPDATE Employee SET EmployeeFirstName = ?, EmployeeLastName = ?, Salary = ?, "
+                "Gender = ?, DOB = ?, Manager = ?, JobSite = ? WHERE Employee.id = ?;",
+                (firstName, lastName, Salary, Gender, DOB, Manager, JobSite, id)
+            )
+            db.commit()
+            flash("Update successful.")
+            return redirect(url_for('sites.index'))
+    return render_template('sites/edit.html', data=data)
+
+
+def getEmployeeData(id):
+    data = get_db().execute(
+        "SELECT * FROM Employee JOIN JobSite ON JobSite.id = Employee.JobSite WHERE Employee.id = ?;",
+        (id,)
+    ).fetchone()
+    if data is None:
+        abort(404, f"Employee {id} doesn't exist.")
+    return data
+
+
+@bp.route('/delete/<int:id>', methods=['POST', 'GET'])
+@login_required
+def delete(id):
+    getEmployeeData(id)
+    db = get_db()
+    db.execute("DELETE FROM Employee WHERE id = ?;", (id,))
+    db.commit()
+    return redirect(url_for('sites.index'))
